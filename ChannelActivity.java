2da6e12677dd93ea5ce730a7665a24b2d770e84a
zaidAnees <98654966+zaidAnees@users.noreply.github.com>
2022-09-15 21:50:41 +0530

    Add files via upload

---
+++ ChannelActivity.java
@@ -0,0 +1,152 @@
+package com.example.chattutorial;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.TextView;
+
+import androidx.activity.OnBackPressedCallback;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.chattutorial.databinding.ActivityChannelBinding;
+import com.getstream.sdk.chat.viewmodel.MessageInputViewModel;
+import com.getstream.sdk.chat.viewmodel.messages.MessageListViewModel;
+import com.getstream.sdk.chat.viewmodel.messages.MessageListViewModel.Mode.Normal;
+import com.getstream.sdk.chat.viewmodel.messages.MessageListViewModel.Mode.Thread;
+import com.getstream.sdk.chat.viewmodel.messages.MessageListViewModel.State.NavigateUp;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import io.getstream.chat.android.client.ChatClient;
+import io.getstream.chat.android.client.events.TypingStartEvent;
+import io.getstream.chat.android.client.events.TypingStopEvent;
+import io.getstream.chat.android.client.models.Channel;
+import io.getstream.chat.android.client.models.Message;
+import io.getstream.chat.android.client.models.User;
+import io.getstream.chat.android.ui.message.input.viewmodel.MessageInputViewModelBinding;
+import io.getstream.chat.android.ui.message.list.adapter.viewholder.attachment.AttachmentFactoryManager;
+import io.getstream.chat.android.ui.message.list.header.MessageListHeaderView;
+import io.getstream.chat.android.ui.message.list.header.viewmodel.MessageListHeaderViewModel;
+import io.getstream.chat.android.ui.message.list.header.viewmodel.MessageListHeaderViewModelBinding;
+import io.getstream.chat.android.ui.message.list.viewmodel.MessageListViewModelBinding;
+import io.getstream.chat.android.ui.message.list.viewmodel.factory.MessageListViewModelFactory;
+
+public class ChannelActivity extends AppCompatActivity {
+
+    private final static String CID_KEY = "key:cid";
+
+    public static Intent newIntent(Context context, Channel channel) {
+        final Intent intent = new Intent(context, ChannelActivity.class);
+        intent.putExtra(CID_KEY, channel.getCid());
+        return intent;
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Step 0 - inflate binding
+        ActivityChannelBinding binding = ActivityChannelBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        String cid = getIntent().getStringExtra(CID_KEY);
+        if (cid == null) {
+            throw new IllegalStateException("Specifying a channel id is required when starting ChannelActivity");
+        }
+
+        // Step 1 - Create three separate ViewModels for the views so it's easy
+        //          to customize them individually
+        ViewModelProvider.Factory factory = new MessageListViewModelFactory.Builder()
+                .cid(cid)
+                .build();
+        ViewModelProvider provider = new ViewModelProvider(this, factory);
+        MessageListHeaderViewModel messageListHeaderViewModel = provider.get(MessageListHeaderViewModel.class);
+        MessageListViewModel messageListViewModel = provider.get(MessageListViewModel.class);
+        MessageInputViewModel messageInputViewModel = provider.get(MessageInputViewModel.class);
+
+        // Set a view factory manager for Imgur attachments
+        ImgurAttachmentFactory imgurAttachmentFactory = new ImgurAttachmentFactory();
+
+        List<ImgurAttachmentFactory> imgurAttachmentViewFactories = new ArrayList<ImgurAttachmentFactory>();
+        imgurAttachmentViewFactories.add(imgurAttachmentFactory);
+
+        AttachmentFactoryManager attachmentFactoryManager = new AttachmentFactoryManager(imgurAttachmentViewFactories);
+        binding.messageListView.setAttachmentFactoryManager(attachmentFactoryManager);
+
+
+        // Step 2 - Bind the view and ViewModels, they are loosely coupled so it's easy to customize
+        MessageListHeaderViewModelBinding.bind(messageListHeaderViewModel, binding.messageListHeaderView, this);
+        MessageListViewModelBinding.bind(messageListViewModel, binding.messageListView, this, true);
+        MessageInputViewModelBinding.bind(messageInputViewModel, binding.messageInputView, this);
+
+        // Step 3 - Let both MessageListHeaderView and MessageInputView know when we open a thread
+        messageListViewModel.getMode().observe(this, mode -> {
+            if (mode instanceof Thread) {
+                Message parentMessage = ((Thread) mode).getParentMessage();
+                messageListHeaderViewModel.setActiveThread(parentMessage);
+                messageInputViewModel.setActiveThread(parentMessage);
+            } else if (mode instanceof Normal) {
+                messageListHeaderViewModel.resetThread();
+                messageInputViewModel.resetThread();
+            }
+        });
+
+        // Step 4 - Let the message input know when we are editing a message
+        binding.messageListView.setMessageEditHandler(messageInputViewModel::postMessageToEdit);
+
+        // Step 5 - Handle navigate up state
+        messageListViewModel.getState().observe(this, state -> {
+            if (state instanceof NavigateUp) {
+                finish();
+            }
+        });
+
+        // Step 6 - Handle back button behaviour correctly when you're in a thread
+        MessageListHeaderView.OnClickListener backHandler = () -> {
+            messageListViewModel.onEvent(MessageListViewModel.Event.BackButtonPressed.INSTANCE);
+        };
+        binding.messageListHeaderView.setBackButtonClickListener(backHandler);
+        getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                backHandler.onClick();
+            }
+        });
+        // Custom typing info header bar
+        TextView typingHeaderView = findViewById(R.id.typingHeaderView);
+        String nobodyTyping = "nobody is typing";
+        typingHeaderView.setText(nobodyTyping);
+
+// Observe raw events through the low-level client
+        Set<String> currentlyTyping = new HashSet<>();
+        ChatClient.instance()
+                .channel(cid)
+                .subscribeFor(
+                        this,
+                        new Class[]{TypingStartEvent.class, TypingStopEvent.class},
+                        event -> {
+                            if (event instanceof TypingStartEvent) {
+                                User user = ((TypingStartEvent) event).getUser();
+                                currentlyTyping.add(user.getName());
+                            } else if (event instanceof TypingStopEvent) {
+                                User user = ((TypingStopEvent) event).getUser();
+                                currentlyTyping.remove(user.getName());
+                            }
+
+                            String typing = "nobody is typing";
+                            if (!currentlyTyping.isEmpty()) {
+                                typing = "typing: " + TextUtils.join(", ", currentlyTyping);
+                            }
+
+                            typingHeaderView.setText(typing);
+                        }
+                );
+
+    }
+}
---
+++ MainActivity.java
@@ -0,0 +1,87 @@
+package com.example.chattutorial;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.chattutorial.databinding.ActivityMainBinding;
+
+import org.jetbrains.annotations.Nullable;
+
+import io.getstream.chat.android.client.ChatClient;
+import io.getstream.chat.android.client.api.models.FilterObject;
+import io.getstream.chat.android.client.logger.ChatLogLevel;
+import io.getstream.chat.android.client.models.Filters;
+import io.getstream.chat.android.client.models.User;
+import io.getstream.chat.android.offline.model.message.attachments.UploadAttachmentsNetworkType;
+import io.getstream.chat.android.offline.plugin.configuration.Config;
+import io.getstream.chat.android.offline.plugin.factory.StreamOfflinePluginFactory;
+import io.getstream.chat.android.ui.channel.list.viewmodel.ChannelListViewModel;
+import io.getstream.chat.android.ui.channel.list.viewmodel.ChannelListViewModelBinding;
+import io.getstream.chat.android.ui.channel.list.viewmodel.factory.ChannelListViewModelFactory;
+
+import static java.util.Collections.singletonList;
+
+public final class MainActivity extends AppCompatActivity {
+
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Step 0 - inflate binding
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Step 1 - Set up the OfflinePlugin for offline storage
+        StreamOfflinePluginFactory streamOfflinePluginFactory = new StreamOfflinePluginFactory(
+                new Config(
+                        true,
+                        true,
+                        true,
+                        UploadAttachmentsNetworkType.NOT_ROAMING
+                ),
+                getApplicationContext()
+        );
+
+        // Step 2 - Set up the client for API calls with the plugin for offline storage
+        ChatClient client = new ChatClient.Builder("b67pax5b2wdq", getApplicationContext())
+                .withPlugin(streamOfflinePluginFactory)
+                .logLevel(ChatLogLevel.ALL) // Set to NOTHING in prod
+                .build();
+
+        // Step 3 - Authenticate and connect the user
+        User user = new User();
+        user.setId("tutorial-droid");
+        user.setName("Tutorial Droid");
+        user.setImage("https://bit.ly/2TIt8NR");
+
+        client.connectUser(
+                user,
+                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoidHV0b3JpYWwtZHJvaWQifQ.NhEr0hP9W9nwqV7ZkdShxvi02C5PR7SJE7Cs4y7kyqg"
+        ).enqueue();
+
+        // Step 4 - Set the channel list filter and order
+        // This can be read as requiring only channels whose "type" is "messaging" AND
+        // whose "members" include our "user.id"
+        FilterObject filter = Filters.and(
+                Filters.eq("type", "messaging"),
+                Filters.in("members", singletonList(user.getId()))
+        );
+
+        ViewModelProvider.Factory factory = new ChannelListViewModelFactory.Builder()
+                .filter(filter)
+                .sort(ChannelListViewModel.DEFAULT_SORT)
+                .build();
+
+        ChannelListViewModel channelsViewModel =
+                new ViewModelProvider(this, factory).get(ChannelListViewModel.class);
+
+        // Step 5 - Connect the ChannelListViewModel to the ChannelListView, loose
+        //          coupling makes it easy to customize
+        ChannelListViewModelBinding.bind(channelsViewModel, binding.channelListView, this);
+        binding.channelListView.setChannelItemClickListener(
+                channel -> startActivity(ChannelActivity.newIntent(this, channel))
+        );
+
+    }
+}
